cb37e962084221277ad21d310195510cb4ab06e4
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-10-05 20:30:36 +0900

    Pass migration
    User クラスを migration 中で定義しておくことでエラーにならない

--- db/migrate/20131005111923_add_name_to_user.rb
+++ db/migrate/20131005111923_add_name_to_user.rb
@@ -1,6 +1,10 @@
 class AddNameToUser < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+  end
+
   def change
     add_column :users, :name, :string
+    User.reset_column_information
     reversible do |dir|
       dir.up { User.update_all name: 'alice' }
     end
