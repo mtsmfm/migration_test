7619adb1005c97a83da126b81eb82cf79585a2da
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-10-05 20:17:47 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hogefuga::Application.config.secret_key_base = 'ad9d321cea54be6daf1babe668eb78e449423b8b85d7382b9e443f54a3a77b4657ab4d90fdb9570054b2a9d7af6dde62f6568ecd58f7a5602ce8e189bcb4c4d8'
